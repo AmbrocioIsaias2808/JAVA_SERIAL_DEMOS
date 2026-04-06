ff2b07be64eea7a87871d4d0c8b11676aba5d084
unknown <ambrocioisaia98>
2026-04-06 10:03:52 -0500

    REPECION

---
+++ RECEPCION/serial_recepcion/src/serial_recepcion/Serial_recepcion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package serial_recepcion;
+
+/**
+ *
+ * @author isaias
+ */
+public class Serial_recepcion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
